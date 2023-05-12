807018e3345596d711b4055f9a58b2764c4c611d
Ocante António <63821194+Ocante@users.noreply.github.com>
2023-05-12 01:46:42 -0300

    Commit de solução da conversão de temperatura

---
+++ ConversorTemperaturas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConversorTemperaturas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Celsius, Fahrenheit, Kelvin;
+            Console.WriteLine("###  CONVERSOR DE TEMPERATURAS  ###");
+            Console.WriteLine("Insira a temperatura em Celsius ");
+            Celsius = double.Parse(Console.ReadLine());
+            Console.WriteLine("--------------------------------------");
+
+
+            //(Celsius * 9 / 5) + 32 = Fahrenheit
+            Fahrenheit = (Celsius * 9/ 5) +32;
+
+            // Celsius + 273,15
+            Kelvin = Celsius + 273.15;
+
+            Console.WriteLine(Celsius + " graus Celcius = " + Fahrenheit + " graus Fahrenheit ");
+            Console.WriteLine(Celsius + " graus Celcius = " + Kelvin + " graus Kelvin ");
+            Console.WriteLine("--------------------------------------");
+            Console.ReadKey();
+
+        }
+    }
+}
